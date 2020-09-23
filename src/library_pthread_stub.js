6e59f366ca7ad9eb0695a032120f704c84116dba
Sam Clegg <sbc@chromium.org>
2020-09-23 08:40:48 -0700

    Remove unused i64 math helpers
    The two that were used were only used in library_pthread_stub.js
    so move them there.

--- src/library_pthread_stub.js
+++ src/library_pthread_stub.js
@@ -180,6 +180,34 @@ var LibraryPThreadStub = {
     return ret;
   },
 
+  //============================
+  // i64 math helpers
+  //============================
+
+  i64Add__sig: 'iiiii',
+  i64Add: function(a, b, c, d) {
+    /*
+      x = a + b*2^32
+      y = c + d*2^32
+      result = l + h*2^32
+    */
+    a = a|0; b = b|0; c = c|0; d = d|0;
+    var l = 0, h = 0;
+    l = (a + c)>>>0;
+    h = (b + d + (((l>>>0) < (a>>>0))|0))>>>0; // Add carry from low word to high word on overflow.
+    {{{ makeStructuralReturn(['l|0', 'h'], true) }}};
+  },
+
+  i64Subtract__sig: 'iiiii',
+  i64Subtract: function(a, b, c, d) {
+    a = a|0; b = b|0; c = c|0; d = d|0;
+    var l = 0, h = 0;
+    l = (a - c)>>>0;
+    h = (b - d)>>>0;
+    h = (b - d - (((c>>>0) > (a>>>0))|0))>>>0; // Borrow one from high word to low word on underflow.
+    {{{ makeStructuralReturn(['l|0', 'h'], true) }}};
+  },
+
   // gnu atomics
 
   __atomic_is_lock_free: function(size, ptr) {
